e349fbaf85e5013c8e7df83201847d40505bbedb
Daniel carvajal <carvajal629@gmail.com>
2025-03-15 14:44:23 -0600

    Primer commit

---
+++ src/main/java/com/example/projectadmin/ProjectManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementToolApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/projectadmin/classes/Greeting.java
@@ -0,0 +1,5 @@
+package com.example.projectadmin.classes;
+
+public record Greeting(long id, String content) {
+        
+}
