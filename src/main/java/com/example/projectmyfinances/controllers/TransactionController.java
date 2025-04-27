86ccda4940c7794303442ae8aee8d266e4cae27b
Daniel carvajal <carvajal629@gmail.com>
2025-04-27 12:32:58 -0600

    changes in transaction controller to return a json response

--- src/main/java/com/example/projectmyfinances/controllers/TransactionController.java
+++ src/main/java/com/example/projectmyfinances/controllers/TransactionController.java
@@ -4,13 +4,16 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Optional;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +28,6 @@ import com.example.projectmyfinances.services.CategoryService;
 import com.example.projectmyfinances.services.TransactionService;
 import com.example.projectmyfinances.services.UserProfileService;
 import com.example.projectmyfinances.services.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/private/transaction")
@@ -99,10 +100,17 @@ public class TransactionController {
 
             Transaction createdTransaction = transactionService.createTransaction(transaction);
 
-            return ResponseEntity.ok("Transaction created successfully with ID: " + createdTransaction.getTransactionId());
+            // Return a JSON object with the transaction ID and a message
+            Map<String, Object> response = new HashMap<>();
+            response.put("transactionId", createdTransaction.getTransactionId());
+            response.put("message", "Transaction created successfully");
+
+            return ResponseEntity.ok(response);
         }
         catch(Exception ex){;
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Request is not valid.");
+            Map<String, Object> error = new HashMap<>();
+            error.put("error", "Request is not valid.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
         }
         
     }
